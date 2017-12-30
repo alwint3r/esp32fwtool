8cb01985ea136b4ee68fa69ee08963dacdab8612
Alwin Arrasyid <alwin.ridd@gmail.com>
2017-12-30 16:30:57 +0700

    Supports both ESP-IDF with or without arduino as component. Added command line options. Added .editorconfig file

--- README.md
+++ README.md
@@ -12,14 +12,46 @@ Tool for dealing with ESP32 firmware binary. Only support projects built with ES
 
 * GNU/Linux Ubuntu
 
+## Supported Python Version
+
+Currently, only Python v2 is supported. I plan to add the support for both v3 and v2.
+
 ## How to Use
+
+For complete usage information, run the command below:
+
+```
+esp32fwtool.py --help
+```
 
 **Combining binary files**
 
-Currently, this tool can only be used if your ESP-IDF project uses [Arduino for ESP32](https://github.com/espressif/arduino-esp32) as the component.
-
-Inside the project directory, simply run:
+* Generating binary file from an ESP-IDF only (without Arduino) project
 
 ```
-esp32fwtool.py
+esp32fwtool.py .
 ```
+
+or
+
+```
+esp32fwtol.py /path/to/espidf/project
+```
+
+* Generating binary file from an ESP-IDF + Arduino as component project.
+
+```
+esp32fwtool.py -a -n "arduino-esp32" . 
+```
+
+or 
+
+```
+esp32fwtool.py --use-arduino --arduino-directory-name "arduino-esp32" .
+```
+
+Where `arduino-esp32` is the default name for the arduino for esp32 directory inside `components` directory. If you use different name for the arduino directory then you must change the command line argument as well.
+
+By default, the tool will generate a binary file called `firmware.bin` inside `firmware` directory inside the current working directory.
+
+If you want to change the output directory and output filename, then you must provide an arguments to `--output-dir` and `--output-filename` respectively.
--- esp32fwtool.py
+++ esp32fwtool.py
@@ -1,34 +1,77 @@
 #!/usr/bin/env python
 
 import os
+import sys
+import argparse
 
-def main():
-    base_dir = os.getcwd()
-    project_name = os.path.basename(base_dir)
+__version__ = '0.1.0'
 
-    # let's assume that build directory is exist
-    # also assume that the project is using arduin as one of the components
-    boot_app0_path = os.path.join(base_dir, 'components/arduino-esp32/tools/partitions/boot_app0.bin')
-    bootloader_path = os.path.join(base_dir, 'build/bootloader/bootloader.bin')
-    app_path = os.path.join(base_dir, 'build/%s.bin' % (project_name))
-    default_path = os.path.join(base_dir, 'build/default.bin')
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='ESP32 firmware tool')
+    cwd = os.getcwd()
+    default_output_dir = os.path.join(cwd, 'firmware')
+    default_output_filename = 'firmware.bin'
 
-    boot_app0_offset = 0xe000
-    bootloader_offset = 0x1000
-    app_offset = 0x10000
-    default_offset = 0x8000
+    parser.add_argument('-a', '--use-arduino', action='store_true', dest='use_arduino', default=False, help='Use Arduino for ESP32 as component')
+    parser.add_argument('-n', '--arduino-directory-name', action='store', dest='arduino_directory_name', default='arduino-esp32', help='Directory name of Arduino component')
+    parser.add_argument('--output-dir', action='store', dest='firmware_output_dir', default=default_output_dir, help='Output directory')
+    parser.add_argument('--output-filename', action='store', dest='firmware_output_filename', default=default_output_filename, help='Output filename')
+    parser.add_argument('project_directory', action='store', default=cwd, help='ESP32 project directory')
 
-    firmware_ingredients = [
-        (bootloader_path, bootloader_offset),
-        (default_path, default_offset),
-        (boot_app0_path, boot_app0_offset),
-        (app_path, app_offset),
+    return parser.parse_args()
+
+def find_partition_binary(project_dir):
+    sdkconfig_file = open(os.path.join(project_dir, 'sdkconfig'))
+
+    for line in sdkconfig_file.readlines():
+        if line.startswith('CONFIG_PARTITION_TABLE_FILENAME'):
+            key, value = line.strip().split('=')
+            value = value.replace('\"', '')
+
+            return value.split('.')[0]
+
+    return None
+
+
+def get_arduino_ingredients(project_name, project_dir, arduino_component):
+    return [
+        (os.path.join(project_dir, 'build/bootloader/bootloader.bin'), 0x1000),
+        (os.path.join(project_dir, 'build/default.bin'), 0x8000),
+        (os.path.join(project_dir, 'components/%s/tools/partitions/boot_app0.bin' % (arduino_component)), 0xe000),
+        (os.path.join(project_dir, 'build/%s.bin' % project_name), 0x10000),
     ]
 
-    firmware_directory = os.path.join(base_dir, 'esp32fw')
-    firmware_out_file = os.path.join(firmware_directory, 'firmware.bin')
+def get_idf_only_ingredients(project_name, project_dir):
+    partition_binary_file = find_partition_binary(project_dir)
+    if partition_binary_file is None:
+        return []
 
-    os.mkdir(firmware_directory)
+    return [
+        (os.path.join(project_dir, 'build/bootloader/bootloader.bin'), 0x1000),
+        (os.path.join(project_dir, 'build/%s.bin' % partition_binary_file), 0x8000),
+        (os.path.join(project_dir, 'build/%s.bin' % project_name), 0x10000),
+    ]
+
+def main():
+    parse_result = parse_arguments()
+
+    project_dir = os.getcwd() if parse_result.project_directory == '.' else parse_result.project_directory
+    project_name = os.path.basename(project_dir)
+
+    firmware_ingredients = []
+    if parse_result.use_arduino:
+        firmware_ingredients = get_arduino_ingredients(project_name, project_dir, parse_result.arduino_directory_name)
+    else:
+        firmware_ingredients = get_idf_only_ingredients(project_name, project_dir)
+
+    if len(firmware_ingredients) < 1:
+        sys.exit(-1)
+
+    firmware_directory = parse_result.firmware_output_dir
+    firmware_out_file = os.path.join(firmware_directory, parse_result.firmware_output_filename)
+
+    if not os.path.isdir(firmware_directory):
+        os.mkdir(firmware_directory)
 
     current_offset = 0x1000
     with open(firmware_out_file, 'wb') as fout:
@@ -41,7 +84,7 @@ def main():
                 data = fin.read()
                 fout.write(data)
                 current_offset += len(data)
-            print("%s - %d" % (bin_path, bin_offset))
+            print('%s - %d' % (bin_path, bin_offset))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
